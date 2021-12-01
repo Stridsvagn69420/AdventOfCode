e4ebb4cd3ba07580969fbdbf519bd85723bbb0b4
Stridsvagn69420 <Stridsvagn69420@users.noreply.github.com>
2021-12-01 21:10:34 +0100

    Remove browser.js + Part 1 is finished

--- 2021/01/day1.ps1
+++ 2021/01/day1.ps1
@@ -1,28 +1,23 @@
 # Get Input for this day
-Import-Module -Name "$PSScriptRoot\..\..\lib\lib.psm1"
-$sessionID = Get-Content "$PSScriptRoot\..\..\lib\session.txt"
-$inputAoC = (Get-Input -Day 1 -Year 2021 -Cookie $sessionID) -split "`n";
-Out-File -InputObject $inputAoC -Encoding "utf8" -FilePath "input.txt"
+$inputAoC;
+if (Test-Path "$PSScriptRoot\input.txt" -PathType Leaf) {
+    Write-Host "Using local input.txt...";
+	$inputAoC = Get-Content "$PSScriptRoot\input.txt";
+} else {
+    Write-Host "Downloading input...";
+    Import-Module -Name "$PSScriptRoot\..\..\lib\lib.psm1";
+	$sessionID = Get-Content "$PSScriptRoot\..\..\lib\session.txt"
+	$inputAoC = (Get-Input -Day 1 -Year 2021 -Cookie $sessionID) -split "`n";
+	Out-File -InputObject ($inputAoC.Where({ $_ -ne ""}) -join "`n") -Encoding "utf8" -FilePath "$PSScriptRoot\input.txt" -NoNewline;
+}
 
-# Main
-$up = 0;
-$down = 0;
+# Part 1
+$up = 1;
 for ($i = 0; $i -lt $inputAoC.Count; $i++) {
     $first = $inputAoC[$i];
     $second = $inputAoC[$i + 1];
-    Write-Host "Index: $i"
-    Write-Host "First: $first";
-    Write-Host "Second: $second";
     if ($first -lt $second) {
         $up++;
-        Write-Host "UP!!!"
     }
-    if ($first -gt $second) {
-        $down++;
-        Write-Host "DOWN!!!"
-    }
-    Write-Host "`n";
 }
 Write-Host "Increased: $up";
-Write-Host "Decreased: $down";
-Write-Host "Total differnces: $($up + $down)";
