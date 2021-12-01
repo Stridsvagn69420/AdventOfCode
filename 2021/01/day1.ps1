a810d1be00faa29d4f693563e9b3424a13f9078a
Stridsvagn69420 <67603294+Stridsvagn69420@users.noreply.github.com>
2021-12-01 22:48:28 +0100

    Part 1 + 2 are finished now, also found the bug
    Bug TL;DR:
    Part 1 had this weird issue that I had an offset of 1, and Part 2 (looks similar to part 1) had the same result as Part 1.
    Turns out PowerShell didn't use integers exclusively...

--- 2021/01/day1.ps1
+++ 2021/01/day1.ps1
@@ -2,22 +2,46 @@
 $inputAoC;
 if (Test-Path "$PSScriptRoot\input.txt" -PathType Leaf) {
     Write-Host "Using local input.txt...";
-	$inputAoC = Get-Content "$PSScriptRoot\input.txt";
+    $inputAoC = Get-Content "$PSScriptRoot\input.txt";
 } else {
     Write-Host "Downloading input...";
     Import-Module -Name "$PSScriptRoot\..\..\lib\lib.psm1";
-	$sessionID = Get-Content "$PSScriptRoot\..\..\lib\session.txt"
-	$inputAoC = (Get-Input -Day 1 -Year 2021 -Cookie $sessionID) -split "`n";
-	Out-File -InputObject ($inputAoC.Where({ $_ -ne ""}) -join "`n") -Encoding "utf8" -FilePath "$PSScriptRoot\input.txt" -NoNewline;
+    $sessionID = Get-Content "$PSScriptRoot\..\..\lib\session.txt"
+    $inputAoC = (Get-Input -Day 1 -Year 2021 -Cookie $sessionID) -split "`n";
+    Out-File -InputObject ($inputAoC.Where({ $_ -ne ""}) -join "`n") -Encoding "utf8" -FilePath "$PSScriptRoot\input.txt" -NoNewline;
 }
 
 # Part 1
-$up = 1;
-for ($i = 0; $i -lt $inputAoC.Count; $i++) {
-    $first = $inputAoC[$i];
-    $second = $inputAoC[$i + 1];
-    if ($first -lt $second) {
-        $up++;
+function Get-Part1 {
+    [int]$up = 0;
+    for ($i = 0; $i -lt $inputAoC.Count; $i++) {
+        [int]$first = $inputAoC[$i];
+        [int]$second = $inputAoC[$i + 1];
+        if ($first -lt $second) {
+            $up++;
+        }
     }
+    Write-Host "Increased: $up";
 }
-Write-Host "Increased: $up";
+
+# Part 2
+function Get-Part2 {
+    [int]$up = 0;
+    for ($i = 0; $i -lt $inputAoC.Count; $i++) {
+        [int]$one = $inputAoC[$i];
+        [int]$two = $inputAoC[$i+1];
+        [int]$three = $inputAoC[$i+2];
+        [int]$four = $inputAoC[$i+3];
+
+        [int]$first = $one + $two + $three;
+        [int]$second = $two + $three + $four;
+        if ($first -lt $second) {
+            $up++;
+        }
+    }
+    Write-Host "Larger: $up";
+}
+
+# Main()
+Get-Part1;
+Get-Part2;
