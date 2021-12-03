b6ea3b00779dd3318e485362b57852cc94ce6b44
Stridsvagn69420 <Stridsvagn69420@users.noreply.github.com>
2021-12-03 20:46:30 +0100

    Code doesn't properly get character majority...

--- 2021/3/index.js
+++ 2021/3/index.js
@@ -24,25 +24,30 @@ if (fs.existsSync(join(__dirname, "input.txt"))) {
 }
 
 // Main()
-function Main(data ) {
+function Main(data) {
 	const list = data.toString().split("\n").filter(x => x.length > 0);
 	if (!fs.existsSync(join(__dirname, "input.txt"))) fs.writeFileSync(join(__dirname, "input.txt"), list.join("\n"));
+	// START BUG: Code counts ones and zeroes wrong
 	var gamma = "";
 	var epsylon = "";
 	for (let i = 0; i < list.length; i++) {
 		let one = 0;
+		let zero = 0;
 		const entry = list[i].split("");
 		for (int = 0; int < entry.length; int++) {
+			console.log("Entry:", i, "Position:", int, "Charcter:", entry[int]);
+			console.log("Before:", one, zero);
 			if (entry[int] == "1") one++;
+			if (entry[int] == "0") zero++;
+			console.log("After:", one, zero);
 		}
-		gamma += (one > (entry.length / 2)) ? "1" : "0";
-		epsylon += (one < (entry.length / 2)) ? "1" : "0";
+		gamma += (one > zero) ? "1" : "0";
+		epsylon += (one < zero) ? "1" : "0";
 	}
-	console.log(gamma);
-	console.log(epsylon);
-	const decGamma = parseInt(gamma)
-	const decEpsyl = parseInt(epsylon);
-	console.log(decGamma);
-	console.log(decEpsyl);
-	console.log(decGamma * decEpsyl);
+	// END BUG: 
+	console.log(gamma, epsylon);
+	const decGamma = parseInt(gamma, 2)
+	const decEpsyl = parseInt(epsylon, 2);
+	console.log(decGamma, decEpsyl);
+	console.log(`Part 1: ${decGamma * decEpsyl}`);
 }
